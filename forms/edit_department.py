fbe01b82437b13a47cffb4815e721f76afe345b4
Kirill <nemollirik@gmail.com>
2022-03-16 01:10:03 +0500

    Упрощённый выбор главных

--- forms/edit_department.py
+++ forms/edit_department.py
@@ -1,11 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, StringField, IntegerField
+from wtforms import SubmitField, StringField, EmailField, SelectField
 from wtforms.validators import DataRequired
 
 
 class EditDepartment(FlaskForm):
     title = StringField('Title of department', validators=[DataRequired()])
-    chief = IntegerField('Chief', validators=[DataRequired()])
-    members = StringField('Members', validators=[DataRequired()])
-    email = StringField('Department Email', validators=[DataRequired()])
+    chief = SelectField('Chief', validators=[DataRequired()], coerce=int,
+                        choices=[(int(i.split()[0]), f'{i.split()[1]} {i.split()[2]}') for i in
+                                 open("users.txt").read().split('\n')[:-1]])
+    members = StringField('Members id', validators=[DataRequired()])
+    email = EmailField('Department Email', validators=[DataRequired()])
     submit = SubmitField('Edit Department')
